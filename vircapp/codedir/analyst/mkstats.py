21f89288664ab7e82b6ca9666f9bab4543057ad8
nick <nis@citycable.ch>
2019-01-09 23:15:37 +0100

    Add a ticker detail panel + bs4 specific modifications

--- vircapp/codedir/analyst/mkstats.py
+++ vircapp/codedir/analyst/mkstats.py
@@ -35,16 +35,14 @@ import utils
 looptime = int(os.environ.get('analyst_refresh', 2))
 
 #  TODO: find a better structure for periods
-
-lperiods = [ 24 * 60,
-             6 * 60,
-             60,
-             30,
-             15 ,
-             5,  
-             1 ]
-             # 0.5 ] # converted in json, name can't contain a dot.
-           
+# hash key exported  will be "m" + 00key to be exportable in javascript and easilly sortable.
+lperiods = {    24 * 60 : "24h",
+                6 * 60  : "6h",
+                60      : "1h",
+                30      : "30m",
+                15      : "15m",
+                5       : "5m", 
+                1       : "1m" }           
 
 laststats_query = [
   { "$match" : { "isodate":{"$gt" : 9999999999 }}},
@@ -94,8 +92,9 @@ if __name__ == "__main__":
             stats = {}
             collection = pairs.mdbpair(pair)
             # query for all periods
-            for period in lperiods:
-                stats["m" + str(period)] = {}
+            for period in lperiods.keys():
+                pkey = "m{:0>5}".format(period) # keys, must be transposable in javascrit/json
+                stats[pkey] = {}
                 # adapt the query for the current time and the period
                 laststats_query[0]['$match']['isodate']['$gt'] = datetime.utcnow() - timedelta(minutes = period)
                 try :
@@ -109,16 +108,17 @@ if __name__ == "__main__":
                 else:
                     res["oc"] = (res['close'] - res['open']) *100 / res['open']
                     res["range"] = res['high'] - res['low']
-                stats["m" + str(period)] = res
+                res['title'] = lperiods[period]
+                stats[pkey] = res
 
-            lastprice[pair] = stats['m60'].get('close')
-            stats["ticker"] = {"price": lastprice[pair], "pair": pair}
 
             # PUBSUB
             res = db[collection].find_one(sort=[('_id', DESCENDING)])
             if res is None:
                 # no data yet, we skip...
                 continue
+            lastprice[pair] = res['price']
+            stats["ticker"] = {"price": lastprice[pair], "pair": pair, "oc": stats['m01440'].get('oc', "??")}
             last_id = res['_id']
             if ( coll_last_id.get(collection) != last_id):
                 rds.publish('cb:mkt:tick:pubsub', json.dumps({"type": "update", "data": stats}))
--- vircapp/codedir/gui/static/utils.js
+++ vircapp/codedir/gui/static/utils.js
@@ -1,4 +1,4 @@
-
+var ticker_stats = {} //received by sse tickerhandler(), used in fill_ticker_details()
 // sse alerts
 function flashhandler(event) {
 	html = "<div class='alert alert-" + event.type +
@@ -7,6 +7,7 @@ function flashhandler(event) {
 	$(html).prependTo('#flash');
 }
 
+// new sse alerts
 function messagehandler(event) {
 	 var obj = JSON.parse(event.data);
 	html = "<div class='alert alert-" + obj.level +
@@ -15,8 +16,10 @@ function messagehandler(event) {
 	$(html).prependTo('#flash');
 }
 
+// updates data in the ticker panel
 function tickerhandler(event) {
 	data = JSON.parse(event.data);
+	ticker_stats[data.ticker.pair] = data;
 
 	pticker = '#panel-ticker-' + data.ticker.pair;
 	ptprice = pticker + " .panel-ticker-price";
@@ -33,14 +36,45 @@ function tickerhandler(event) {
 	$(pticker).css("background-color", bgcol);
 	$(pticker).fadeOut("slow");
 	$(ptprice).text(data.ticker.price);
-	$(pticker + " .panel-ticker-oc").text(Number((data.m1440.oc).toFixed(2)) + "%"); 
-	if ( data.m1440.oc >= 0){
+	$(pticker + " .panel-ticker-oc").text(Number((data.ticker.oc).toFixed(2)) + "%"); 
+	if ( data.ticker.oc >= 0){
 		$(ptoc).css("color", "lawnGreen");
 	} else {
 		$(ptoc).css("color", "red");
 	}
+	if ($("#ticker-detail-pair").text() == data.ticker.pair){
+		fill_ticker_details(data.ticker.pair);
+	}
 	$(pticker).fadeIn("fast")
 	setTimeout(function() { $(pticker).css(nstyle) ; }, 2000);
+}
+
+// opens the detailled ticker panel
+function open_ticker_details(pair) {
+	$("#ticker-panel").addClass("ticker-panel-open");
+	$("#ticker-panel").removeClass("ticker-panel-mini");
+	$("#ticker-details").removeClass("d-none");
+	fill_ticker_details(pair);
+}
+
+function fill_ticker_details(pair){
+	var stats = ticker_stats[pair];
+	$("#ticker-detail-tbody").empty();
+	$("#ticker-detail-pair").text(stats.ticker.pair);
+	for ( k in stats) {
+		if ( k == "ticker")   { continue; }
+		html = "<tr><th>" + stats[k].title + "</th><td>" + stats[k].low +
+				"</td><td>" + stats[k].high + "</td><td>" + stats[k].volume.toFixed(2) + 
+				"</td><td>" + stats[k].oc.toFixed(2) + "%</td></tr>";
+		$(html).appendTo('#ticker-detail-tbody');
+	}
+}
+
+function close_ticker_details(){
+	$("#ticker-panel").removeClass("ticker-panel-open");
+	$("#ticker-panel").addClass("ticker-panel-mini");
+	$("#ticker-details").addClass("d-none");
+	$("#ticker-detail-pair").text("");
 }
 
 $(window).on('beforeunload', function(){
